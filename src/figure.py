ee85250beecc290f62c5a5d8b0dff8af98a49482
user <user@gmail.com>
2023-05-16 16:04:00 +0300

    script main.py for lesson 3

--- src/figure.py
+++
@@ -1,8 +0,0 @@
-class Figure:
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def add_area(self, figure: 'Figure') -> int:
-        if not isinstance(figure, Figure):
-            raise ValueError('Invalid figure')
-        return self.area + figure.area
