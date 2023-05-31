e4680f44d5959c98d45aa998c2568f22fc6c2fea
user <user@gmail.com>
2023-05-31 23:03:32 +0300

    fixed breed tests

--- lesson_7/test_01_dog_api.py
+++ lesson_7/test_01_dog_api.py
@@ -20,65 +20,29 @@ def test_GET_single_random_image_from_all_dogs_collection_200():
     assert response.json() != [], 'Got empty response'
 
 
-@pytest.mark.parametrize('random_value', [-1, 0, 'abc', 50, 51])
+@pytest.mark.parametrize('random_value', [1, 'abc'])
 def test_GET_multiple_random_images_from_all_dogs_collection_200(random_value):
     response = requests.get(
         f"{dog_url}/breeds/image/random/{random_value}")
-    if random_value == -1:
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert len(response.json().get("message")) == 1, 'Wrong message values'
-    elif random_value == 0:
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert len(response.json().get("message")) == 1, 'Wrong message values'
-    elif random_value == 'abc':
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert len(response.json().get("message")) == 1, 'Wrong message values'
-    elif random_value == 50:
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert len(response.json().get("message")
-                   ) == 50, 'Wrong message values'
-    elif random_value == 51:
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert len(response.json().get("message")
-                   ) == 50, 'Wrong message values'
+    assert response.status_code == 200, 'Wrong status code'
+    assert response.json() != [], 'Got empty response'
 
 
-@pytest.mark.parametrize('breed', [0, 'abc', "affenpinscher"])
-def test_GET_multiple_images_by_breed_200_404(breed):
-    response_404_error = "Breed not found (master breed does not exist)"
-    response = requests.get(
-        f"{dog_url}/breed/{breed}/images")
-    if breed == 0:
-        assert response.status_code == 404, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert response.json().get("message") == response_404_error, 'Wrong message values'
-    elif breed == 'abc':
-        assert response.status_code == 404, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert response.json().get("message") == response_404_error, 'Wrong message values'
-    elif breed == 'affenpinscher':
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
+def get_breed_ids():
+    response = requests.get(f"{dog_url}/breeds/list/all")
+    breed_ids = list(response.json().get("message").keys())[:2]
+    return breed_ids
 
 
-@pytest.mark.parametrize('breed', [0, 'abc', "affenpinscher"])
-def test_GET_single_random_image_by_breed_200_404(breed):
-    response_404_error = "Breed not found (master breed does not exist)"
-    response = requests.get(
-        f"{dog_url}/breed/{breed}/images/random")
-    if breed == 0:
-        assert response.status_code == 404, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert response.json().get("message") == response_404_error, 'Wrong message values'
-    elif breed == 'abc':
-        assert response.status_code == 404, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
-        assert response.json().get("message") == response_404_error, 'Wrong message values'
-    elif breed == 'affenpinscher':
-        assert response.status_code == 200, 'Wrong status code'
-        assert response.json() != [], 'Got empty response'
+@pytest.mark.parametrize('breed_id', get_breed_ids())
+def test_GET_multiple_images_by_breed_200(breed_id):
+    response = requests.get(f"{dog_url}/breed/{breed_id}/images")
+    assert response.status_code == 200, 'Wrong status code'
+    assert response.json() != [], 'Got empty response'
+
+
+@pytest.mark.parametrize('breed_id', [1, 'abc'])
+def test_GET_multiple_images_by_breed_404(breed_id):
+    response = requests.get(f"{dog_url}/breed/{breed_id}/images")
+    assert response.status_code == 404, 'Wrong status code'
+    assert response.json() != [], 'Got empty response'
